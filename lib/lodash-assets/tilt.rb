34bfb392e2407053d6cf28f76c884c6e77df34e8
Jesse Stuart <jesse@jessestuart.ca>
2013-05-03 20:00:43 -0400

    Show template name in compile errors

--- lib/lodash-assets/tilt.rb
+++ lib/lodash-assets/tilt.rb
@@ -4,11 +4,17 @@ module LoDashAssets
   class Tilt < Tilt::Template
     self.default_mime_type = 'application/javascript'
 
+    CompileError = Class.new(StandardError)
+
     def evaluate(scope, locals, &block)
       template_namespace = LoDashAssets.template_namespace
       template_name = scope.logical_path.gsub(%r{\A#{LoDashAssets.path_prefix}/}, '')
 
-      compiled_template = LoDash.compile(data)
+      begin
+        compiled_template = LoDash.compile(data)
+      rescue ExecJS::RuntimeError => e
+        raise CompileError.new("Failed to compile #{template_name}: #{e.message}")
+      end
 
       <<-TEMPLATE
         this.#{template_namespace} || (this.#{template_namespace} = {});
